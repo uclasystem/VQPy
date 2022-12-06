8f83214294dd9b65650ddfd0fd0746cb1cd74e44
Shan Yu <shanyu1@g.ucla.edu>
2022-12-06 01:25:33 +0000

    add people_counting_per_frame example

---
+++ examples/people_counting/people_counting_per_frame.py
@@ -0,0 +1,73 @@
+import argparse
+import numpy as np
+import vqpy
+from vqpy.function.logger import vqpy_func_logger
+from yolox_detector import YOLOXDetector
+from vqpy.detector.logger import register
+
+
+def make_parser():
+    parser = argparse.ArgumentParser('VQPy Demo!')
+    parser.add_argument('--path', help='path to video')
+    parser.add_argument(
+        "--save_folder",
+        default=None,
+        help="the folder to save the final result",
+    )
+    parser.add_argument(
+        "-d",
+        "--pretrained_model_dir",
+        help="Directory to pretrained models")
+    return parser
+
+
+class Person(vqpy.VObjBase):
+    pass
+
+
+class PersonOnCrosswalk(vqpy.QueryBase):
+
+    @staticmethod
+    def setting() -> vqpy.VObjConstraint:
+
+        CROSSWALK_REGION_1 = [(731, 554), (963, 564), (436, 1076), (14, 1076)]
+        CROSSWALK_REGION_2 = [(1250, 528), (1292, 473),
+                              (1839, 492), (1893, 547)]
+        CROSSWALK_REGIONS = [CROSSWALK_REGION_1, CROSSWALK_REGION_2]
+
+        def get_bottom_central_point(tlbr):
+            x = (tlbr[0] + tlbr[2]) / 2
+            y = tlbr[3]
+            return (x, y)
+
+        def on_crosswalk(tlbr):
+            from shapely.geometry import Point, Polygon
+            bottom_central_point = get_bottom_central_point(tlbr)
+            # print(f"tlbr is {tlbr}")
+            # print(f"bottom central_point {bottom_central_point}")
+            point = Point(bottom_central_point)
+            for region in CROSSWALK_REGIONS:
+                poly = Polygon(region)
+                if point.within(poly):
+                    return True
+            return False
+
+        filter_cons = {"__class__": lambda x: x == Person,
+                       "tlbr": on_crosswalk}
+        select_cons = {"track_id": None,
+                       }
+        return vqpy.VObjConstraint(filter_cons=filter_cons,
+                                   select_cons=select_cons,
+                                   filename='on_crosswalk')
+
+
+if __name__ == '__main__':
+    args = make_parser().parse_args()
+    register("yolox", YOLOXDetector, "yolox_x.pth")
+    vqpy.launch(cls_name=vqpy.COCO_CLASSES,
+                cls_type={"person": Person},
+                tasks=[PersonOnCrosswalk()],
+                video_path=args.path,
+                save_folder=args.save_folder,
+                detector_name="yolox",
+                detector_model_dir=args.pretrained_model_dir)
