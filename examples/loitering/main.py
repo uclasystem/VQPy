4f3dd2eb3ee185eaa83672fde574fd4fb56a65f2
Hanry <54668181+hanryxu@users.noreply.github.com>
2022-12-13 21:05:34 +0000

    Use built-in function within_regions and property bottom_center

--- examples/loitering/main.py
+++ examples/loitering/main.py
@@ -33,26 +33,12 @@ class People_loitering_query(vqpy.QueryBase):
         REGION = [
             (550, 550), (1162, 400), (1720, 720), (1430, 1072), (600, 1073)
             ]
-
-        def get_bottom_central_point(tlbr):
-            x = (tlbr[0] + tlbr[2]) / 2
-            y = tlbr[3]
-            return (x, y)
-
-        def in_region_of_interest(tlbr):
-            from shapely.geometry import Point, Polygon
-
-            botton_central_point = get_bottom_central_point(tlbr)
-            point = Point(botton_central_point)
-            poly = Polygon(REGION)
-            if point.within(poly):
-                return True
-            return False
+        REGIONS = [REGION]
 
         filter_cons = {
             "__class__": lambda x: x == Person,
-            "tlbr": vqpy.utils.lasting(
-                trigger=in_region_of_interest, time=10, name="in_roi"
+            "bottom_center": vqpy.utils.lasting(
+                trigger=vqpy.utils.within_regions(REGIONS), time=10, name="in_roi"
             ),
         }
         select_cons = {
