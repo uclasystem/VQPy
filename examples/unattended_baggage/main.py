0c5582e35cd24be7ab0fb8173972b2e05cae0a21
Hanry <54668181+hanryxu@users.noreply.github.com>
2023-01-06 21:29:15 +0000

    Add @stateful to Baggage's property owner

--- examples/unattended_baggage/main.py
+++ examples/unattended_baggage/main.py
@@ -32,6 +32,7 @@ class Person(vqpy.VObjBase):
 
 
 class Baggage(vqpy.VObjBase):
+    @vqpy.stateful()
     @vqpy.cross_vobj_property(
         vobj_type=Person, vobj_num="ALL",
         vobj_input_fields=("track_id", "tlbr")
