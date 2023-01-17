d018801b07f0f09ea7751fa43573118297ecc892
Hanry <54668181+hanryxu@users.noreply.github.com>
2023-01-17 04:18:55 +0000

    Use YOLOX as default detector

--- vqpy/__init__.py
+++ vqpy/__init__.py
@@ -29,8 +29,8 @@ def launch(cls_name,
            video_path: str,
            save_folder: str = None,
            save_freq: int = 10,
-           detector_model_dir: str = "yolox",
-           detector_name: str = None,
+           detector_model_dir: str = None,
+           detector_name: str = "yolox",
            ):
     """Launch the VQPy tasks with specific setting.
     Args:
